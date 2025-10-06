fc76d62b9a8611b7d2751bf2b4412353a10cdb9d
umutkamanoglu <kamanogluu@gmail.com>
2025-10-06 20:53:11 +0300

    Dashboard'a başlandı.

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <main className='bg-gradient-to-tr p-10 py-10 max-sm:py-px from-zinc-900 via-black to-zinc-900 min-h-screen overflow-auto text-white'>
+            <header className='flex h-24 flex-col md:flex-row items-start md:items-center justify-between bg-gray-900/50 backdrop-blur-sm rounded-xl p-6 border border-gray-800 gap-4'>Header</header>
+        </main>
+    )
+}
+
+export default Dashboard
